f92fe716cf9f24fe77f6a011f3d27e142f642b8d
CossyApple <cossyapple5@gmail.com>
2026-08-26 16:41:09 -0500

    Added Bookmarklet script

---
+++ Bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:(function()%7Bvar%20s=document.createElement('script');s.src='https://cdn.jsdelivr.net/gh/cossyapple5-prog/UnderChrome@main/UnderChrome.js';document.body.appendChild(s);%7D)();
